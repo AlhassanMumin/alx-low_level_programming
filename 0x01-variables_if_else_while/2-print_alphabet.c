2956dc3f0c728c20d480f3d040eab9ebe53bdabc
Mumin8 <alhassanmumin8@gmail.com>
2022-07-01 15:35:13 -0700

    I some times suffer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+	char lc;
+	for (lc = 'a'; lc <= 'z'; lc++)
+		putchar(lc + "\n");
+
+	return (0);
+	
+}
+			
