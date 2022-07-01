9806d9f4c03f0d67e988d8225d3fd1df52d412b9
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-01 16:19:22 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,6 +14,7 @@ int main(void)
 {
 
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -23,7 +24,7 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
+
 	return (0);
 }
 
