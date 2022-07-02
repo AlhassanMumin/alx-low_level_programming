d2b3c07b3aa65845128ef9904b8936ef830d9dd6
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-02 03:12:19 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	return 0;
+	int d;
+
+	for (d = '0'; d <= '8'; d++)
+	{
+		for (e = '1'; e <= '9'; e++)
+		{
+			
+			putchar(d);
+			putchar(e);
+			if (d != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		
+		
+	}
+	putchar('\n');
+
+	return (0);
 }
