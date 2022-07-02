42a65897fff4e022abca687e1b869ed2e4fd52d6
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-02 00:52:03 +0000

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,9 +4,10 @@
 int main(void)
 {
 	char emit_e_q;
+	
 	for (emit_e_q = 'a'; emit_e_q <= 'z'; emit_e_q++)
 	{
-		if (emit_e_q == 'e'|| emit_e_q == 'q')
+		if (emit_e_q == 'e' || emit_e_q == 'q')
 		{
 		}
 		else
