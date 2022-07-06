17ecd253999c9f6a51256124dc8a9eb283be24b7
Mumin8 <alhassanmumin8@gmail.com>
2022-07-05 20:21:24 -0700

    alphabe

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,18 +4,17 @@
  */
 void print_alphabet_x10(void)
 {
-	char alphabet;
-	int counter = 0;
+char alphabet;
+int counter = 0;
 
-	while (counter < 10)
-	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		{
-			_putchar(alphabet);
-		}
-		counter = counter + 1;
-
-	 	_putchar('\n');
-	}
+while (counter < 10)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+counter = counter + 1;
+_putchar('\n');
+}
 
 }
