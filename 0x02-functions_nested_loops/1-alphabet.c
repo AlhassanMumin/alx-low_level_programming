29dc36e3888a79f93f6ff88dc9855521aa3d5a2b
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-06 00:10:55 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,18 @@
-#include "main.h"
-
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
+	char lower_case;
+	char nl = '\n';
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	putchar(nl);
+
+	return (0);
 }
