8b9fc9a63561d42bc48c9a6b9dafa2820362b802
Mumin8 <alhassanmumin8@gmail.com>
2022-07-06 15:10:58 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs -  computes the absolute value of a number
+ * @val: The number to be computed
+ * Return: Always an absolute value
+ */
+int _abs(int val)
+{
+	return (abs(val));
+}
