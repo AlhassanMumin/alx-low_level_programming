dd8770c05bb666acac8de30bb435f996780ce21a
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-07 00:16:39 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,29 @@
 #include "main.h"
+/**
+ * times_table - This will print times table from 0 to 9
+ */
+void times_table(void)
+{
+	int count, multiplier, result;
+
+	for (count = 0; count <= 9; count++)
+	{
+		_putchar('0');
+
+		for (multiplier = 1; multiplier <= 9; multiplier++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			result = count * multiplier;
+
+			if (result <= 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
