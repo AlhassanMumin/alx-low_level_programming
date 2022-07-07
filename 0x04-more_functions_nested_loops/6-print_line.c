62c8408ddb0664264a4dcb6513bb6b8964d7537a
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-07 22:57:16 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_line - Draws a straight line 
- * @n: The number that determine the length of line.
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of _ characters to be printed.
  */
 void print_line(int n)
 {
