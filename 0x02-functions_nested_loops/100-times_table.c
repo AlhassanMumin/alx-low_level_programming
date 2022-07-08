f290d53695baa7469f99d7a39f7c618032d8be5c
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-08 17:46:09 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,8 +35,8 @@ void print_times_table(int n)
 
 				if (k >= 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10)) % 10 + '0');
 				}
 				else if (k <= 99 && k >= 10)
 				{
