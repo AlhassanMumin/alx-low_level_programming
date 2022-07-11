293a6634fb4fa9b121677b4b947c8bc7d5b60d1f
Mumin8 <alhassanmumin8@gmail.com>
2022-07-11 16:31:54 -0700

    print upper half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - prints half of the string
+ * @str: the pointer to the string
+ */
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	if (len % 2 == 0)
+	{
+		for (i = (len - (len / 2)); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (len - 1 - (len / 2)); i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+
