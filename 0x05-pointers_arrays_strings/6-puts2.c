2770a5ffeda2493a377c2f90b8afdef7d0c467bc
Mumin8 <alhassanmumin8@gmail.com>
2022-07-11 15:44:36 -0700

    print some characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts2 - prints any other character
+ * @str: the pointer to the string of characters
+ */
+void puts2(char *str)
+{
+	int len, i;
+	
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
