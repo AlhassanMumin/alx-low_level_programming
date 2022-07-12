ba7dd5711cfb049aa0040ec556670be30771ba15
Mumin8 <alhassanmumin8@gmail.com>
2022-07-11 18:35:05 -0700

    copy with strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * *_strcpy - It copies a string pointd to by scr to a buffer
+ * pointed to by dest
+ * @src: The pointer to string
+ * @dest: The pointer to buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
+
