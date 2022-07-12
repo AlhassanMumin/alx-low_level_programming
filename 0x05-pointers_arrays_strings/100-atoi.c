e6ec5e024d727ae87ba9049a4cc3d996bb719153
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-12 02:06:30 +0000

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,22 @@
-#
+{
+	unsigned int val = 0;
+	int k = 1;
+	
+
+	do {
+		if (*s == '-')
+		{
+			k*= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			val = (val * 10) + (*s - '0');
+		}
+		else if (val > 0)
+		{
+			break;
+		}
+	} while (*s++);
+
+	return (val * k);
+}
