3f9303deb8150a3e76ab158fcfc8122ce27d545a
Mumin8 <alhassanmumin8@gmail.com>
2022-07-13 03:40:07 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * strcat - A func that will concatenate a string
+ * @src: A pointer to source s2
+ * @dest: A pointe to s2
+ * @Return: The string to be returned
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
