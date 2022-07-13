e37a8d699d760e10ed7b4a56cd1d2b8e8683e40f
Mumin8 <alhassanmumin8@gmail.com>
2022-07-13 12:53:41 -0700

    concatenate with the function strncat

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,9 +6,11 @@
  * @dest: The pointer to buffer
  * @src: The pointer to source
  * @n: The number of bytes to copy
+ * Return: The pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
+
 	return (dest);
 }
