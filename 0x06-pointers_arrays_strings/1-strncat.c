b0e3e0f967cf808cc673362f1b0b5b541d10f672
Mumin8 <alhassanmumin8@gmail.com>
2022-07-13 07:31:02 -0700

    concatenate with strncat function);

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strncat - a func that concatenate two strings with strncat()
+ *
+ * @dest: The pointer to the string that will be copied to
+ * @src: The pointer to the sring to be copied
+ * @n: The number of characters to copy
+ */
+ char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
