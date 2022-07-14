9a5b8c68c2104a8523d9bd908a67ca4c981d504d
Mumin8 <alhassanmumin8@gmail.com>
2022-07-13 17:53:56 -0700

    capitalize the string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include <string.h>
+/**
+ * cap_string - this function witll capitalize a string
+ * @ptr: the pointer to the string to be capitalize
+ * Return: the capitalized string
+ */
+char *cap_string(char *ptr)
+{
+	int len, i;
+
+	len = strlen(ptr);
+	if (isalpha(ptr[0]))
+	{
+		ptr[0] = toupper(ptr[0]);
+	}
+	for (i = 1; i < len; i++)
+	{
+		if(isalpha(ptr[i]) && ptr[i - 1] == ' ')
+		{
+			ptr[i] = toupper(ptr[i]);
+		}
+	}
+	return (ptr);
+}
