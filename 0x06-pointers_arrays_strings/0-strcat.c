7b8f109b62e18cae00a9051e0eb20c06a6855ff8
Mumin8 <alhassanmumin8@gmail.com>
2022-07-13 17:57:56 -0700

    concatenate the string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat - A func that will concatenate a string
  * @src: A pointer to source s2
  * @dest: A pointe to s2
- * @Return: The string to be returned
+ * Return: The string to be returned
  */
 char *_strcat(char *dest, char *src)
 {
