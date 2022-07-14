f9eb8ce281be35de1eedd61269038ba8d2e95f8c
Mumin8 <alhassanmumin8@gmail.com>
2022-07-13 19:31:51 -0700

    Encode the string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include <string.h>
+/**
+ * leet - it will encode the string
+ * @p: the string to be encoded
+ * Return: pointer to the encoded string
+ */
+char *leet(char *p)
+{
+	int i, len;
+
+	len = strlen(p);
+	for (i = 0; i < len; i++)
+	{
+		if (p[i] == 'a' || p[i] == 'A')
+		{
+			p[i] = 4 + '0';
+		}
+		else if (p[i] == 'e' || p[i] == 'E')
+		{
+			p[i] = 3 + '0';
+		}
+		else if (p[i] == 'o' || p[i] == 'O')
+		{
+			p[i] = 0 +'0';
+		}
+		else if (p[i] == 't' || p[i] == 'T')
+		{
+			p[i] = 7 + '0';
+		}
+		else if (p[i] == 'l' || p[i] == 'L')
+		{
+			p[i] = 1 + '0';
+		}
+	}
+	return (p);
+}
