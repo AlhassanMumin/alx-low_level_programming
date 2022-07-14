3244507f409f649176259f2ba67f76d6535c2c6b
Mumin8 <alhassanmumin8@gmail.com>
2022-07-14 16:51:44 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <string.h>
+/**
+ * rot13 - it will encode string using rot13
+ * @s: the string that will be encoded
+ * Return: pointer to s
+ */
+char *rot13(char *s)
+{
+	int len, i, temp;
+
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+		if (isalpha(s[i]) && ((s[i] >= 'a' && s[i] <= 'z') ||
+		(s[i] >= 'A' && s[i] <= 'Z')))
+		{
+			temp = s[i + 13];
+			while ((s[i] >= 'N' && s[i] <= 'z') || 
+			(s[i] >= 'N' && s[i] <= 'Z'))
+			{
+				temp = s[i - 26];
+			}
+			s[i] = temp;
+		}
+	}
+	return (s);
+}
