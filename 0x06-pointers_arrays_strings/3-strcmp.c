6baa60bd194e9ce32d1d0a30343970f96ec26f6c
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-14 09:36:36 +0000

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,6 +5,7 @@
  *
  * @s1: The value to be compared to
  * @s2: The value to compare to
+ *
  * Return: 1 if  s1 is higheter than s2
  * 	   0 if s1 and s2 are same
  * 	   -1 if s2 is higher than s1
