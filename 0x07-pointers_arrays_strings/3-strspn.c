7a1260ff2356a814d47498beddd7f4620dd100e2
Mumin8 <alhassanmumin8@gmail.com>
2022-07-18 10:38:40 -0700

    length of prefixed substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strspn - the function calculate in byte the initial segment of
+ * string s
+ * @s: the stirng which iniial segment will be calculated
+ * @accept: the string which determine how many bytes will be calculated
+ * in string s
+ * Return: the number of bytes in the initial segment of string s
+ */
+unsigned  int _strspn(char *s, char *accept)
+{
+	unsigned int byt =strspn(s, accept);
+	return (byt);
+}
