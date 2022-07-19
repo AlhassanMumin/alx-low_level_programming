77aa9b5c249cec4db6022524065fb4e06fcc1818
Mumin8 <alhassanmumin8@gmail.com>
2022-07-19 10:40:00 -0700

    crackme password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,1 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-/**
- * main - the entry point. it will generate a random
- * password anytime the program is executed
- *
- * Return: - Always 0
- */
-int main (void)
-{
-	srand(time(0));
-	printf("%d",rand());
-	return (0);
-}
+abc123
