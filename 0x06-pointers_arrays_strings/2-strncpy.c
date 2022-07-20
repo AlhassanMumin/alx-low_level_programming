484dec3d1d048aae3f4ce32015caa218048fbbef
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-07-20 00:35:45 +0000

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,18 @@
  * @n: The number of bytes to copy
  * Return: The pointer to dest
  */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i, j;
-
-	i = 0, j = 0;
-	while (n)
+char *_strncpy(char *dest, char *src, int n)                                                                  
+{                                                                                                                
+	int i;                                                                                                    
+                                                                                                                            
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[j] = src[i];
-		j++, i++, n--;
+		dest[i] = src[i];
 	}
-	return (dest);
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+                                                                                                                            
+	return (dest);                                                                                                 
 }
