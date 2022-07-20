51f892ece0852cb709d2c9b5cbb5f6cf8624ace5
Mumin8 <alhassanmumin8@gmail.com>
2022-07-20 16:06:57 -0700

    the length of the string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,12 @@ int _strlen_recursion(char *s)
 	len = 0;
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	if (*s)
 	{
-		_strlen_recursion(s + 1);
 		len = len + 1;
+		len = len + _strlen_recursion(s + 1);
 	}
 	return (len);
 }
