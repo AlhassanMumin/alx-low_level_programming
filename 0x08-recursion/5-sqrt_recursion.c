ac9d841bd94125b4b4b2c882a5a837a4bb53550d
Mumin8 <alhassanmumin8@gmail.com>
2022-07-21 05:13:17 -0700

    Natural square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,27 @@
 /**
+ * sqrt_func - it returns to the caller function
+ * @a: the argument which square root to be determined
+ * @b: the base case
+ * Return: it will return result to  the caller
+ */
+int sqrt_func(int a, int b)
+{
+	if (b *b == a)
+	{
+		return (b);
+	}
+	else if(b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt_func(a, b + 1));
+}
+/**
  * _sqrt_recursion - it will check for a number with natural square root
  * @n: the number to check
  * Return: the sqare root value
  */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	for (i = 1; i < n; i++)
-	{
-		if (i * i == n)
-		{ 
-			i = i;
-		}
-		else
-		{
-			i = -1;
-		}
-	}
-return (i);
+return (sqrt_func(n, 1));
 }
