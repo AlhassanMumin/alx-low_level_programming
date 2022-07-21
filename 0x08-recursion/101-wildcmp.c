284435a862583056171531054e5436330865627d
Mumin8 <alhassanmumin8@gmail.com>
2022-07-21 14:22:52 -0700

    compare strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,8 +4,7 @@
  * @s1: the first string
  * @s2: the second string
  * Return: 1 if s1 is greater
- * 	   0 if s1 and s2 are same
- * 	   -1 if s2 is greater
+ * 0 if s1 and s2 are same, -1 if s2 is greater
  */
 int wildcmp(char *s1, char *s2)
 {
