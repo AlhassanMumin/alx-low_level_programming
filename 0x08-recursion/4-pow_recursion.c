912b85e7a9e0398a2392d7715bda38035a464bf8
Mumin8 <alhassanmumin8@gmail.com>
2022-07-20 17:32:12 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - it raises a number to some power
+ * @x: the base number
+ * @y: the exponent 
+ * Return: the result of power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y-1));
+}
+
