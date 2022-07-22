7842fe89f0d24c20c832dd8ba9ac5dd2a8e497cc
Mumin8 <alhassanmumin8@gmail.com>
2022-07-22 15:00:53 -0700

    multiply two values

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the stating point
+ * @argc: the argument count
+ * @argv: the strings passed from command line
+ * Return: Always 0  on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Errro \n");
+		return (1);
+	}
+	return (0);
+}
