a5ea5b0f3e66bbb5ffbfa94892e12baea7a658a7
Mumin8 <alhassanmumin8@gmail.com>
2022-07-22 14:14:02 -0700

    print your name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the stating point
+ * @argc: the argument count
+ * @argv: the strings passed from command line
+ * Return: Always 0  on success
+ */
+int main(int __attribute__ ((unused))  argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
