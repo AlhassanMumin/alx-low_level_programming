1ae23fbe1270aa8f7d1b938bafe40869542460a9
Mumin8 <alhassanmumin8@gmail.com>
2022-07-22 14:29:51 -0700

    number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the stating point
+ * @argc: the argument count
+ * @argv: the strings passed from command line
+ * Return: Always 0  on success
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
