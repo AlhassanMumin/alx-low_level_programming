8a216bb2f55d6cdcd6a882d3e44d6e55ec3141d6
Mumin8 <alhassanmumin8@gmail.com>
2022-07-23 14:15:10 -0700

    add arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main - the stating point
+ * @argc: the argument count
+ * @argv: the strings passed from command line
+ * Return: Always 0  on success
+ */
+int main(int argc, char *argv[])
+{
+	int result, i;
+	
+	result = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			result = result + result;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
