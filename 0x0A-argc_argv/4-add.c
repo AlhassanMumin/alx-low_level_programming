be9b7341f4bf6760548319ca37ac1a2e9c0c1a66
Mumin8 <alhassanmumin8@gmail.com>
2022-07-23 15:03:53 -0700

    Add values

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
 /**
  * main - the stating point
@@ -9,20 +8,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, i;
+	int result, i, j;
 
 	result = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i] < '0' || argv[i] == '9')
+		for (j =0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			result = result + atoi(argv[i]);
-		}
+		result = result + atoi(argv[i]);
+		
 	}
 	printf("%d\n", result);
 	return (0);
