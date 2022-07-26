1a16f1c861b9cf0cf982d5063249e054e9e890d0
Mumin8 <alhassanmumin8@gmail.com>
2022-07-26 15:31:09 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - it will allocate a pointer to a 2D array
+ * @width: the width of the 2D array
+ * @height: the height of the 2D array
+ * Return: Pointer to a 2D array or NULL if failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **d_array;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	d_array = malloc(sizeof(int *) * width);
+	if (d_array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		d_array[i] = (int *)malloc(sizeof(int *) * height);
+		if (d_array[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			d_array[i][j] = 0;
+		}
+	}
+	return (d_array);
+	free(d_array);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,4 +3,6 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *);
 char *str_concat(char *, char *);
+int **alloc_grid(int width, int height);
+int _putchar(char c);
 #endif
