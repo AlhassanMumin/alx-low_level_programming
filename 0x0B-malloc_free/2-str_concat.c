1aa6b09f544dacb9d836ebc83537c94a1d1cb5c4
Mumin8 <alhassanmumin8@gmail.com>
2022-07-26 08:31:09 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,9 +10,13 @@ char *str_concat(char *s1, char *s2)
 {	char *concat;
 
 	if (s1 == NULL)
-		s1 = "\0";
-	else if (s2 == NULL)
-		s2 = "\0";
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	concat = (char *)malloc(sizeof(char) * strlen(s1)
 		 + sizeof(char) * strlen(s2) + 1);
 	if (concat == NULL)
@@ -22,4 +26,5 @@ char *str_concat(char *s1, char *s2)
 	strcat(concat, s1);
 	strcat(concat, s2);
 	return (concat);
+	free(concat);
 }
