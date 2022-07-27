9a81af1c99de1e38c6500d833fdbb165d56f0be3
Mumin8 <alhassanmumin8@gmail.com>
2022-07-27 10:02:15 -0700

    Change in cents

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,49 +8,47 @@
  */
 int main(int  argc, char *argv[])
 {
-	int count, bal, div, money;
+	int count, bal, div;
 
 	count = 0,
-	money = atoi(argv[1]);
-	bal = money;
+	bal = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-		while (bal)
+	while (bal > 0)
+	{
+		if (bal >= 25)
 		{
-			if (bal >= 25)
-			{
-				div = bal / 25;
-				bal = bal - div * 25;
-				count = count + div;
-			}
-			if (bal >= 10)
-			{
-				div = bal / 10;
-				bal = bal - div * 10;
-				count = count + div;
-			}
-			if (bal >= 5)
-			{
-
-				div = bal / 5;
-				bal = bal - div * 5;
-				count = count + div;
-			}
-			if (bal >= 2)
-			{
-				div = bal / 2;
-				bal = bal - div * 2;
-				count = count + div;
-			}
-			if (bal <= 1)
-			{
-				count = count + bal;
-				bal = bal - bal;
-			}
+			div = bal / 25;
+			bal = bal - div * 25;
+			count = count + div;
 		}
+		if (bal >= 10)
+		{
+			div = bal / 10;
+			bal = bal - div * 10;
+			count = count + div;
+		}
+		if (bal >= 5)
+		{
+			div = bal / 5;
+			bal = bal - div * 5;
+			count = count + div;
+		}
+		if (bal >= 2)
+		{
+			div = bal / 2;
+			bal = bal - div * 2;
+			count = count + div;
+		}
+		if (bal <= 1)
+		{
+			count = count + bal;
+			bal = bal - bal;
+		}
+	}
 	printf("%d\n", count);
 	return (0);
 }
