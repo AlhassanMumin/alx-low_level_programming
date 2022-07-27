ed010bb7cd109a6c911b487ca22893d4adc8256f
Mumin8 <alhassanmumin8@gmail.com>
2022-07-27 08:31:45 -0700

    multiply commandline arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,15 @@ int main(int argc, char *argv[])
 	int result, i;
 
 	result = 1;
-	if (argc > 1)
+	if (argc != 3)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			result *= atoi(argv[i]);
-		}
-		printf("%d\n", result);
-	}
-	else
-	{
-		printf("Errro\n");
+		printf("Error\n");
 		return (1);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		result *= atoi(argv[i]);
+	}
+	printf("%d\n", result);
 	return (0);
 }
