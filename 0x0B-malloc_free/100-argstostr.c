a344483d3a6772a22de9179bbbb8d4b0c0b74570
Mumin8 <alhassanmumin8@gmail.com>
2022-07-27 17:18:44 -0700

    concatenate commandline arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+/**
+ * argstostr - It concatenate all the arguments
+ * @ac: the number of argument passed in
+ * @av: the argument vector
+ * Return: pinter to the new string or NULL if failure
+ */
+ char *argstostr(int ac, char **av)
+{
+	int i, j;
+	char *concat;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	concat = (char *)malloc(sizeof(char *) * (ac + 1));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (; av[i][j]; j++)
+		{
+			if (av[i][j] == ' ')
+			{
+				concat[j] = '\n';
+			}
+			concat[j] = av[i][j];
+		}
+	}
+	free(concat);
+	return (concat);
+}
