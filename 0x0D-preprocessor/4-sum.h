211881bae2e2c1156ad0a23b9f482e2a8d3e1d42
Mumin8 <alhassanmumin8@gmail.com>
2022-08-01 07:09:21 -0700

    sum macro function

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,9 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * SUM - the function that compute sum
+ * @x: first variable to take value
+ * @y: second variable to take value
+ */
+#define SUM(x, y) ((x) + (y));
+#endif
