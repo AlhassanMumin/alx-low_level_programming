e870758d8856723c6077a75a623bd4d7ac307fcf
Mumin8 <alhassanmumin8@gmail.com>
2022-08-01 14:17:10 -0700

    my dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog -this is about my dog
+ * @name: the name of my dog
+ * @age: the age of my dog
+ * @owner: the owner fo the dog
+ */
+ typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
+#endif
