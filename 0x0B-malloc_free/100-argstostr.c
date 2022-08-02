2f7f006c67e3cffe32dc239a5bb366a13e4b2ff3
Mumin8 <alhassanmumin8@gmail.com>
2022-08-02 08:25:47 -0700

    concatenate string

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,22 +9,25 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k;
+	int i, j, k, stlen;
 	char *concat;
 
 	k = 0;
+	for (i = 1; i < ac; i++)
+		for (;av[i][j];)
+			stlen++;
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	concat = malloc(sizeof(char *));
+	concat = malloc(sizeof(char) +(stlen + 1));
 	if (concat == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 1; i < ac; i++)
 	{
-		for (j = 0; strlen(av[i]); j++)
+		for (j = 0;av[i][j]; j++)
 		{
 			concat[k] = av[i][j];
 			k++;
