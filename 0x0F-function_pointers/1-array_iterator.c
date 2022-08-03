758372035775a7b2f339fcdaaf249ae8e345f37f
Mumin8 <alhassanmumin8@gmail.com>
2022-08-03 10:29:23 -0700

    print array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,9 @@
 #include "function_pointers.h"
-/**
- * arrray_iterator -  the array passed in
- * @size: the size of the array
- * @action: pointer to  the function
- */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long i;
 
-	if (array == NULL || size == NULL)
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
