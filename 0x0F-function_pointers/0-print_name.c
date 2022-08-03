1764f8fcc468cd058a3186e3eb154eb5db49df32
Mumin8 <alhassanmumin8@gmail.com>
2022-08-03 06:51:24 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
-void my_name(char *);
 /**
  * print_name -  the function to call my_name functin
  * @name: the name to be printed
@@ -8,18 +6,5 @@ void my_name(char *);
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f = my_name;
 	(*f)(name);
-}
-/**
- * my_name - the function to pirnt  the name
- * @str: the pointer to the character  string
- */
-void my_name(char *str)
-{
-	int i;
-	for (i = 0; str[i]; i++)
-	{
-		_putchar(str[i]);
-	}
 }
