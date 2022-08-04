abd9a2adad5c35a7266892f2e4799b1c5b5120c9
Mumin8 <alhassanmumin8@gmail.com>
2022-08-04 13:38:53 -0700

    sum all the inputs

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,6 +2,7 @@
 /**
  * sum_them_all - it will sum the passed in values
  * @n: the number of values passed in
+ * @...: the variadic notationt 
  * Return: the sum of the values or o0 if no size is specified
  */
 int sum_them_all(const unsigned int n, ...)
