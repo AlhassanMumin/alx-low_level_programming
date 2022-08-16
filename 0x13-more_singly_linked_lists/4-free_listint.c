7791a801c0108150edb2337a0703c42488901fc1
Mumin8 <alhassanmumin8@gmail.com>
2022-08-16 04:56:07 -0700

    the function frees all the memory allocated

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint - the function to free listint_t
+ * @head: the pointer to the first node of the linked list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;								}
+}
