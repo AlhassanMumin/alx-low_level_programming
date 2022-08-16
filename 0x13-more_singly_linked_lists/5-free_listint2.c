2b53732b3870b3d203930f174472e4a930e57697
Mumin8 <alhassanmumin8@gmail.com>
2022-08-16 08:06:54 -0700

    freed all the momory allocated

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - A function that frees a memory
+ * @head: the pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr = *head, *tmp;
+
+	while (curr)
+	{
+		tmp = curr;
+		curr = curr->next;
+		free(tmp);
+	}
+	curr = NULL;
+}
