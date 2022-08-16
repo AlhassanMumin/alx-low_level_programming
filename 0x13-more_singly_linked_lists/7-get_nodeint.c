576db0dbe05055305ad9811d9f05e06685d6e9d6
Mumin8 <alhassanmumin8@gmail.com>
2022-08-16 16:20:42 -0700

    node with a value equal index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - function to get a node at nth index
+ * @head: the pointer to the first node
+ * @index: the index to serarch for in the list
+ * Return: the nth node on success or NULL on failure
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	while (head)
+	{
+		if (head->n == (int)index)
+		{
+			return (head);
+		}
+		head = head->next;
+	}
+	return (NULL);
+}
