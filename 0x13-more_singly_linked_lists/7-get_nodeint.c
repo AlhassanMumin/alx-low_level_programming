12c8fd396e647d241704cdbb8de5c95fb11fc9cb
Mumin8 <alhassanmumin8@gmail.com>
2022-08-16 16:58:41 -0700

    free memory

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,16 +8,13 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	if (head == NULL)
-		return (NULL);
+	unsigned int node;
 
-	while (head)
+	for (node = 0; node < index; node++)
 	{
-		if (head->n == (int)index)
-		{
-			return (head);
-		}
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 	}
-	return (NULL);
+	return (head);
 }
