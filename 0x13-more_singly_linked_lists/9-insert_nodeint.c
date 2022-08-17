cc8a2a93c60dea327b5d0dcae2d4dc8da61e7a2e
Mumin8 <alhassanmumin8@gmail.com>
2022-08-17 14:30:11 -0700

    insert a node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (tmp == NULL)
 		return (NULL);
 	tmp->n = n;
+	if (idx == 0)
+	{
+		tmp->next = *head;
+		*head = tmp;
+		return (*head);
+	}
 	for (i = 0; i < idx; i++)
 	{
 		if (new_node == NULL || new_node->next == NULL)
