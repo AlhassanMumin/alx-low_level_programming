183850ebbd6b98f334ffef9f82bb518b5172f053
Mumin8 <alhassanmumin8@gmail.com>
2022-08-17 05:26:29 -0700

    pop head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,9 +13,8 @@ int pop_listint(listint_t **head)
 	if (*head == NULL)
 		return (0);
 
-	tmp = (*head)->next;
-	free(*head);
-	*head = tmp;
+	tmp = *head;
+	*head = (*head)->next;
 	free(tmp);
 	return (i);
 }
