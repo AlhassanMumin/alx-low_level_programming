a01ee0538f0866e440a0d9a80add2fa69de41d01
Mumin8 <alhassanmumin8@gmail.com>
2022-08-18 15:16:55 -0700

    decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * binary_to_uint - the function to convert binary to decimal
+ * @b: a pointer to the stirng of characters
+ * Return: the converted characters
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0, mul = 1;
+	int len, i = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[i])
+	{
+		len += 1;
+		i++;
+	}
+	len--;
+	for (; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		dec += (b[len] - '0') * mul;
+		mul *= 2;
+	}
+	return (dec);
+}
