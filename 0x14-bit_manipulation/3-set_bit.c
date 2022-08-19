865ab11a1d71ea1a55a701577facc83249734196
Mumin8 <alhassanmumin8@gmail.com>
2022-08-19 16:41:53 -0700

    set a bit at index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,7 +6,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if ((*n | (1 << index)) == 1)
-		return (1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n |= (1 << index);
+
 	return (-1);
 }
