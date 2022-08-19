6611918a04b7a1f0480bf765ee8706c55ac9b728
Mumin8 <alhassanmumin8@gmail.com>
2022-08-18 17:47:47 -0700

    value at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+/**
+ * git_bit - the functionto get the bit at an index
+ * @n: the value to be converteed to some bit values
+ * @index: the index to search for
+ * Return: the value at that index or -1 if error occurs
+ */t
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
+}
