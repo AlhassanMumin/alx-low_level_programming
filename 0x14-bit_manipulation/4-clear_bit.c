1f63a67af65bee0e7a31c6a29ee1d0e06b59723a
Mumin8 <alhassanmumin8@gmail.com>
2022-08-19 17:01:57 -0700

    clear a bit at index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+/**
+ * clear_bit - the function to clear a bit
+ * @n: pointter to the integer
+ * @index: the index at where to clear
+ * Return: 1 on success or -1 if error occurs
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	
+	*n &= ~(1 << index);
+	return (1);
+}
