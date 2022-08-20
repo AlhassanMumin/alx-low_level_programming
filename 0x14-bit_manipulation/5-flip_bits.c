e564cf68e3b750141bd03658df5d08977a285d73
Mumin8 <alhassanmumin8@gmail.com>
2022-08-19 17:36:11 -0700

    flip count

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 /**
- * flip_bits - Counts the number of bits needed to be to
- * 	       be flipped to get from one number to another
- * @n: The number
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
  * @m: The number to flip n to.
  *
  * Return: The necessary number of bits to flip to get from n to m.
