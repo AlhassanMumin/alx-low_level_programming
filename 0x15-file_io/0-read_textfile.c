0fc539efb6dabdc42ca5715d57614cdf52e2c6ae
Mumin8 <alhassanmumin8@gmail.com>
2022-08-24 16:40:18 -0700

    read and write from a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - the function to read and write a file
+ * @filename: the name of the file to read and write
+ * @text_content: the number of characters to read and write
+ * Return: the number of characters that had been read and written
+ */ 
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd = 0, char_read = 0, char_write = 0;
+	char *buf;
+	
+	if (filename == NULL)
+		return (0);
+	
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	char_read = read(fd, buf, letters);
+	if (char_read == -1)
+		return (0);
+	char_write = write(STDOUT_FILENO, buf, char_read);
+	if (char_write == -1)
+		return (0);
+	close(fd);
+	return (char_write);
+}
