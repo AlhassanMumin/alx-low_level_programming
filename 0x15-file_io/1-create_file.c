0a717c02957956d435a622980810ba95d354a193
Mumin8 <alhassanmumin8@gmail.com>
2022-08-25 03:06:24 -0700

    creating a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,13 +4,13 @@
  * create_file -  the function to create a file
  *
  * @filename: the name of the file to create
- * @text_content: the content to write to the text file 
+ * @text_content: the content to write to the text file
  * Return: 1 on success of -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0, fdw;
-	
+
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
@@ -27,6 +27,7 @@ int create_file(const char *filename, char *text_content)
 	if (fdw == -1)
 		return (-1);
 	close(fd);
+
 
 	return (1);
 }
