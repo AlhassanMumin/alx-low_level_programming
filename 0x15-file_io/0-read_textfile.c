0751a27b9e71f787493ea14d6aeff5fd62ca5061
Mumin8 <alhassanmumin8@gmail.com>
2022-08-26 04:10:57 -0700

    reading a file:

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
+	{
+		free(buf);
 		return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
