8b530011fefeb0bed58a7a6c4220631a7880bd37
Mumin8 <alhassanmumin8@gmail.com>
2022-08-26 04:05:35 -0700

    copy  from a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 char *create_buffer(char *file);
 void close_file(int fd);
-
-/**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
 char *create_buffer(char *file)
 {
 	char *buffer;
@@ -26,11 +18,6 @@ char *create_buffer(char *file)
 
 	return (buffer);
 }
-
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
 void close_file(int fd)
 {
 	int c;
@@ -43,19 +30,6 @@ void close_file(int fd)
 		exit(100);
 	}
 }
-
-/**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: 0 on success.
- *
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
- */
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
