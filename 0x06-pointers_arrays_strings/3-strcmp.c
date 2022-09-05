b2ae272b670e75da6cc4873670579f132cb12053
Mumin8 <alhassanmumin8@gmail.com>
2022-09-05 04:27:36 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,11 +5,16 @@
  * @s1: The value to be compared to
  * @s2: The value to compare to
  *
- * Return: 1 if  s1 is higheter than s2
+ * Return: positive value if  s1 is higheter than s2
  * 	   0 if s1 and s2 are same
- * 	   -1 if s2 is higher than s1
+ * 	   negative value if s2 is higher than s1
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(s1, s2));
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
