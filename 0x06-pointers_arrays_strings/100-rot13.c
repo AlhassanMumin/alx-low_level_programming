7d5847dc16c377c2751a2214a042e4ebd3707161
Mumin8 <alhassanmumin8@gmail.com>
2022-09-05 16:42:58 -0700

    rot13 encoded

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,23 +7,29 @@
  */
 char *rot13(char *s)
 {
-	int len, i, temp;
+	int  index, i = 0;
+	char all_alpha[52] = {'A','B','C','D','E','F','G','H','I','J',
+			      'K','L','M','N','O','P','Q','R','S','T',
+			      'U','V','W','X','Y','Z','a','b','c','d',
+			      'e','f','g','h','i','j','k','l','m','n',
+			      'o','p','q','r','s','t','u','v','w','x',
+			      'y','z'};
+	char rot13_key[52] = {'N','O','P','Q','R','S','T','U','V','W',
+			      'X','Y','Z','A','B','C','D','E','F','G',
+			      'H','I','J','K','L','M','n','o','p','q',
+			      'r','s','t','u','v','w','x','y','z','a',
+			      'b','c','d','e','f','g','h','i','j','k',
+			      'l','m'};
 
-	len = strlen(s);
-	for (i = 0; i < len; i++)
+	while (s[i])
 	{
-		if (isalpha(s[i]) && ((s[i] >= 'a' && s[i] <= 'z') ||
-		(s[i] >= 'A' && s[i] <= 'Z')))
+		for (index = 0; index < 52; index++)
 		{
-			temp = s[i + 13];
-			while ((s[i] >= 'n' && s[i] <= 'z') ||
-			(s[i] >= 'N' && s[i] <= 'Z'))
-			{
-				temp = s[i - 26];
-				break;
-			}
-			s[i] = temp;
+			if (s[i] == all_alpha[index])
+				s[i] = rot13_key[index];
 		}
+		i++;
 	}
+
 	return (s);
 }
