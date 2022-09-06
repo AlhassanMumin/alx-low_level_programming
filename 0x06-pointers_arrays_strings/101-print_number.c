e7608232d21ca66e0c5fea1b4039a13638c189d4
Mumin8 <alhassanmumin8@gmail.com>
2022-09-06 02:38:49 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,38 +6,12 @@
  */
 void print_number(int n)
 {
-	int count, pw, rem, j, val, k;
-
 	if (n < 0)
 	{
 		n = n * -1;
 		_putchar('-');
 	}
-	rem = n % 10;
-	pw = 1;
-	count = 0;
-	val = n = n - rem;
-	while (val > 9)
-	{
-		val = floor(val / 10);
-		count = count + 1;
-	}
-	k = count;
-	while (k)
-	{
-		pw = pw * 10;
-		k--;
-	}
-	while (count)
-	{
-		j = floor(n / pw);
-		n = n - (pw * j);
-		pw = pw / 10;
-		_putchar(j + '0');
-		count--;
-	}
-	_putchar(rem + '0');
+	if ((n / 10 > 0))
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
-
-
-
