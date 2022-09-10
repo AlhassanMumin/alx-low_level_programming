2b83607844ba2c18fba5735e7d8b996f3ec9ac84
Mumin8 <alhassanmumin8@gmail.com>
2022-09-10 04:11:40 -0700

    calloc memory allocation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * _calloc - thee function to allocate a memory
+ * @nmemb: the number of blocks of memory to allocate
+ * @size: the size of each block of memory
+ * Return: a pointer to the allocated memory or
+ * 	   NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *caloc;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	caloc = calloc(nmemb, sizeof(size));
+	if (caloc == NULL)
+		return (NULL);
+	return (caloc);
+}
