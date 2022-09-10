bd941b231e65100a71087f416a3c37ae31ebd954
Mumin8 <alhassanmumin8@gmail.com>
2022-09-10 04:17:17 -0700

    calloc memory allocation

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	caloc = calloc(nmemb, sizeof(size));
+	caloc = calloc(nmemb, size);
 	if (caloc == NULL)
 		return (NULL);
 	return (caloc);
