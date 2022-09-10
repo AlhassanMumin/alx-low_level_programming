77d48818461d410e683d540649cd23ea4ceb07c0
Mumin8 <alhassanmumin8@gmail.com>
2022-09-10 06:46:36 -0700

    create and initialize an array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * array_range - the function to create array of integers
+ * @min: the smaller value
+ * @max: the bigger value
+ * Return: pointer to the array created or NULL on failure
+ */
+int *array_range(int min, int max)
+{
+	int dif, *arr, i;
+
+	dif = max - min;
+
+	if (dif < 0)
+		return (NULL);
+	arr = malloc(sizeof(int) * dif + 1);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i <= dif; i++)
+		arr[i] = min++;
+	return (arr);
+}
