706ad159e3f1813b4a3d6cb661d31c184ad96be1
Mumin8 <alhassanmumin8@gmail.com>
2022-09-10 11:28:02 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -68,7 +68,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 
 	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (doggo->owner == NULL)
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
