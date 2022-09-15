a887a3f5fc49b958e8c4dcf84e2eda8c2941ce9e
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2022-09-15 20:57:30 +0000

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * list_len -  the funct to print the list of the nodes
- * @n: the head passed in
+ * @h: the head passed in
  * Return:  the number of nodes in the list
  */
 size_t list_len(const list_t *h)
