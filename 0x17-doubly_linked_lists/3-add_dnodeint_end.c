9792766188e042da676a47f52c4befd7c30f9788
Mumin8 <alhassanmumin8@gmail.com>
2022-09-29 17:19:28 -0700

    doubly linked list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,6 +9,9 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *tmp = *head;
 
+	if (**head == NULL)
+		return NULL;
+
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
