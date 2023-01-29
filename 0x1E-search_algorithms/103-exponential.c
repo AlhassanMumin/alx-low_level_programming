d78676ee62d306697345ee5730ff97fcbfd79c35
Alhassan Mumin <106683956+Mumin8@users.noreply.github.com>
2023-01-29 14:00:01 +0000

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 #include "search_algos.h"
+/**
+* min - find the smaller value
+*
+* @a: the first value
+* @b: the second value
+*
+* Return: the smaller value
+*/
 
 size_t min(size_t a, size_t b)
 {
 	return (b > a ? a : b);
 }
+/**
+  * binary_searcher - Searches for a value in a sorted array
+  *                  of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @L: The starting index of the [sub]array to search.
+  * @R: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  */
 
 int binary_searcher(int *array, int value, size_t L, size_t R)
 {
@@ -29,6 +48,17 @@ int binary_searcher(int *array, int value, size_t L, size_t R)
 	}
 	return -1;
 }
+
+**
+  * exponential_search - Searches for a value in a sorted array
+  *                      of integers using exponential search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  */
 
 int exponential_search(int *array, size_t size, int value)
 {
